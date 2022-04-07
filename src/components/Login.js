233b65d30cb6e7634837f5c95b75152d22a13be8
Michal Kunicki <michalkunicki96@gmail.com>
2022-04-07 15:55:06 +0200

    Implemented API calls and Firebase login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
-import 'firebase/app'
-
-import { auth } from '../firebase'
 import firebase from 'firebase/app'
+import { auth } from '../firebase'
 
-const Login = () => {
+export default function Login() {
   return (
     <div id="login-page">
       <div id="login-card">
@@ -32,5 +30,3 @@ const Login = () => {
     </div>
   )
 }
-
-export default Login
